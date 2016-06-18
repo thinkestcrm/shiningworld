2e2a37dba78b1668dc595b5af0ddf5392ec83474
thinkestcrm <thinkest@163.com>
2016-06-18 10:57:11 +0800

    add jumptable

---
+++ yingtu/library/skiptable.php
@@ -0,0 +1,9 @@
+<?php
+    // actionid url 
+    $skiptable = array(
+        // login.php
+        "1" => array("url" => "logic/login.php"),
+        // inded.php
+        "2" => array("url" => "index.php")
+    );
+?>
