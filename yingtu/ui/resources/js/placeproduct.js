a832d2415eb4e03cd9f482f1da81e0bfbf7a6b68
thinkestcrm <thinkest@163.com>
2016-06-24 17:33:45 +0800

    wenhuan 6.24

---
+++ yingtu/ui/resources/js/placeproduct.js
@@ -0,0 +1,63 @@
+/**
+ * Created by 123 on 2016/6/24.
+ */
+$(function(){
+    $(".his_works_nav,.foodproduct_wrapper,.foodproduct_main,.foodproduct_info_box").css({
+        width:window.screen.width+"px"
+    });
+    $(".supporting_services_addclothing").hover(function(){
+        var addclothing=$(this).clone();
+        $(this).parent("li").prepend(addclothing);
+        $(this).attr("src","../ui/resources/image/AddClothing2.jpg");
+        $(this).css({
+            position:"absolute",
+            top:"0",left:"-160px"
+        });
+        stop(true,true);
+        $(this).animate({
+            left:"0px"
+        }, "slow",function(){
+            $(this).prev().remove();
+            $(this).css({position:"relative"})
+        });
+    },function(){
+
+    });
+    $(".supporting_services_addscenario").hover(function(){
+        var addclothing=$(this).clone();
+        $(this).parent("li").prepend(addclothing);
+        $(this).attr("src","../ui/resources/image/AddScenario2.jpg");
+        $(this).css({
+            position:"absolute",
+            top:"0",left:"-160px"
+        });
+        stop(true,true);
+        $(this).animate({
+            left:"0px"
+        }, "slow",function(){
+            $(this).prev().remove();
+            $(this).css({position:"relative"})
+        });
+    },function(){
+
+    });
+    $(".supporting_services_addmakeup").hover(function(){
+        var addclothing=$(this).clone();
+        $(this).parent("li").prepend(addclothing);
+        $(this).attr("src","../ui/resources/image/AddMakeup2.jpg");
+        $(this).css({
+            position:"absolute",
+            top:"0",left:"-160px"
+        });
+        stop(true,true);
+        $(this).animate({
+            left:"0px"
+        }, "slow",function(){
+            $(this).prev().remove();
+            $(this).css({position:"relative"})
+        });
+    },function(){
+
+    });
+
+}) ;
